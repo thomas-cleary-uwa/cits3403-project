3c018cd4699ba448f2748aec7c133690a305ff34
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-04-22 18:16:50 +0800

    rebuilt db to change extension from bd to db

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'password123'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.bd')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
