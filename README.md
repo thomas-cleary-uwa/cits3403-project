ef9361b1da0f8a2232629e2704de3b9c4b3243c7
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-04-22 15:11:44 +0800

    created base template for flask app

--- README.md
+++ README.md
@@ -1,13 +1,6 @@
 # cits3403-project
 Project for CITS3403 - Semester 1 2021
 
-To run flask app need to set FLASK_APP environement variable 
-to "application_instance.py"
-
-    - export FLASK_APP=application_instance.py
-
-(on Windows use set instead of export)
-
-Now to run 
+To run flask app
 
     - flask run
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Thomas'}
+
+    jobs = [
+        {
+            'person' : {'name' : 'Thomas'},
+            'task' : 'login'
+        },
+        {
+            'person' : {'name' : 'Calvin'},
+            'task' : 'content'
+        },
+        {
+            'person' : {'name' : 'Jason'},
+            'task' : 'home page'
+        },
+        {
+            'person' : {'name' : 'Michael'},
+            'task' : 'quiz'
+        }
+    ]
+
+    return render_template('index.html', user=user, jobs=jobs)
+
+    
