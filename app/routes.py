9cb0c9e0877fcf9b2d0b196767a8cc61e3b85580
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-04-22 20:40:42 +0800

    added user registration

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user, login_required
+from flask import render_template, flash, redirect, url_for, request
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Thomas'}
 
     jobs = [
         {
@@ -28,7 +28,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', user=user, jobs=jobs)
+    return render_template('index.html', jobs=jobs)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -42,7 +42,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
@@ -50,6 +53,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/quiz')
