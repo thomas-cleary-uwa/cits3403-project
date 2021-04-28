98de4baf475d2d515e8a28fc974acd4f5aac1fa6
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-04-28 18:59:13 +0800

    add links in admin page to go back to main site

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_admin.menu import MenuLink
 
 
 app = Flask(__name__)
@@ -23,3 +24,4 @@ from app import routes, models
 admin = Admin(app, name="INSERT NAME HERE", template_mode="bootstrap3")
 
 admin.add_view(models.AdminModelView(models.User, db.session))
+admin.add_link(MenuLink(name="Back to Site", url="/index", category="Links"))
