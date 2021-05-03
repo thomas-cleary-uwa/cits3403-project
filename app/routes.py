f2e309f5aab3e55455e5e6687a01e25cb1851f7b
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-05-03 14:37:17 +0800

    refactored the quiz_questions route

--- app/routes.py
+++ app/routes.py
@@ -95,6 +95,7 @@ def user(username):
 
     return render_template('user.html', user=user, attempts=attempts, average=average, numAttempts=numAttempts)
 
+
 @app.route('/quiz')
 @login_required
 def quiz():
@@ -106,166 +107,27 @@ def quiz():
 def quiz_questions():
     NUM_NON_QUESTION_FIELDS = 3 # 2 Submits and 1 hidden?
 
-    if current_user.has_saved_attempt:
-        saved_attempt = SavedAttempt.query.filter_by(user_id=current_user.id).first()
-        if saved_attempt is None:
-            # do some actual error checking
-            print("error no saved attempt")
-
-        else:
-            # do something not hard coded once this is working
-            questions = [
-                Question.query.get(saved_attempt.question_a_id),
-                Question.query.get(saved_attempt.question_b_id),
-                Question.query.get(saved_attempt.question_c_id)
-            ]
-
-            saved_responses = [
-                saved_attempt.response_a,
-                saved_attempt.response_b,
-                saved_attempt.response_c
-            ]
-
-            # set default values field.default doesn't work
-            form = QuizForm(
-                question1=saved_responses[0],
-                question2=saved_responses[1],
-                question3=saved_responses[2]
-            )
-            
-            fields = [field for field in form]
-            fields = fields[:-NUM_QUESTIONS_IN_QUIZ]
-            index = 0
-            for field in fields:
-                question = questions[index]
-                field.label = question.question
-
-                field.choices = [
-                    (1, question.response_a), 
-                    (2, question.response_b),
-                    (3, question.response_c)
-                ]
-
-                index += 1
-
-
-    else:
-    # REMOVE THIS LOGIC INTO SEPERATE FUNCTION
-    # TO RANDOMISE THIS 
-    # GET NUMBER OF ROWS IN QUESTION TABLE
-    # SELECT RANDOM INT FROM 1->NUM_ROWS FOR NUMBER OF QUESTIONS ON QUIZ
-    # SELECT THESE ROWS FROM THE TABLE TO BE THE QUIZ QUESTIONS
-        # every field except the submit field
-        form = QuizForm()
-        questions = []
-    
-        fields = [field for field in form]
-        fields = fields[:-NUM_QUESTIONS_IN_QUIZ]
-
-        for i in range(1, len(fields)+1):
-            question = Question.query.get(i)
-            questions.append(question)
-
-        index = 0
-        for field in fields:
-            question = questions[index]
-            field.label = question.question
-            # change to allow for as many questions as in the questions list
-            field.choices = [
-                (1, question.response_a), 
-                (2, question.response_b),
-                (3, question.response_c)
-            ]
-
-            index += 1
-
-    print(form.csrf_token)
+    form, questions = create_question_form()
 
     if form.validate_on_submit():
 
         if form.submit.data:
 
-            outcome = 0 # dummy value
-            if Question.query.get(1).answer == form.question1.data: value_a = 1
-            else: value_a = 0
-            if Question.query.get(2).answer == form.question2.data: value_b = 1
-            else: value_b = 0
-            if Question.query.get(3).answer == form.question3.data: value_c = 1
-            else: value_c = 0
-            value_score = value_a + value_b + value_c
+            score = submit_attempt(form, questions)
 
-            attempt = SubmittedAttempt( 
-                user_id=current_user.id,
-                
-                # change this from being hard coded for question id
-                question_a_id = questions[0].id,
-                response_a=form.question1.data, 
-                mark_a=value_a,
-
-                question_b_id = questions[1].id,
-                response_b=form.question2.data, 
-                mark_b=value_b, 
-
-                question_c_id = questions[2].id,
-                response_c=form.question3.data, 
-                mark_c=value_c,
-                    
-                score = value_score,
-                attempt_datetime = datetime.utcnow()
-            )
-            
-            db.session.add(attempt)
-            db.session.commit()
-
-            # NEED TO DELETE ANY SAVED ATTEMPTS IN THE DATABASE FOR THIS USER 
             if current_user.has_saved_attempt:
-                savedAttempts = SavedAttempt.query.filter_by(user_id=current_user.id).all()
+                delete_saved_attempts()
 
-                for attempt in savedAttempts:
-                    db.session.delete(attempt)
-
-                current_user.has_saved_attempt = False
-
-                db.session.commit()
-
-            outcome = value_score
-            return render_template('result.html',form=form, outcome=outcome)
+            return render_template('result.html',form=form, outcome=score)
         
 
     # save button was pressed
     if form.save.data:
 
-        # NEED TO DELETE ANY SAVED ATTEMPTS IN THE DATABASE FOR THIS USER 
         if current_user.has_saved_attempt:
-            savedAttempts = SavedAttempt.query.filter_by(user_id=current_user.id).all()
+            delete_saved_attempts()
 
-            for attempt in savedAttempts:
-                db.session.delete(attempt)
-
-            current_user.has_saved_attempt = False
-
-            db.session.commit()
-
-        savedAttempt = SavedAttempt(
-            user_id=current_user.id,
-            
-            # change this from being hard coded for question id
-            question_a_id = questions[0].id,
-            response_a=form.question1.data, 
-
-            question_b_id = questions[1].id,
-            response_b=form.question2.data, 
-
-            question_c_id = questions[2].id,
-            response_c=form.question3.data, 
-                
-            saved_datetime = datetime.utcnow()
-        )
-
-        db.session.add(savedAttempt)
-        current_user.has_saved_attempt = True
-        db.session.commit()
-
+        save_attempt(form, questions)
 
         return redirect(url_for('quiz'))
 
@@ -281,3 +143,161 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
+
+""" 
+-------------------------------------------------------------------------------
+Below are helper functions for the route functions
+-------------------------------------------------------------------------------
+"""
+
+def delete_saved_attempts():
+        savedAttempts = SavedAttempt.query.filter_by(user_id=current_user.id).all()
+
+        for attempt in savedAttempts:
+            db.session.delete(attempt)
+
+        current_user.has_saved_attempt = False
+
+        db.session.commit()
+
+
+def get_saved_attempt():
+    saved_attempt = SavedAttempt.query.filter_by(user_id=current_user.id).first()
+
+    questions = [
+        Question.query.get(saved_attempt.question_a_id),
+        Question.query.get(saved_attempt.question_b_id),
+        Question.query.get(saved_attempt.question_c_id)
+    ]
+
+    saved_responses = [
+        saved_attempt.response_a,
+        saved_attempt.response_b,
+        saved_attempt.response_c
+    ]
+
+    return(questions, saved_responses)
+
+
+def get_radio_fields(form):
+    fields = [field for field in form]
+    fields = fields[:-NUM_QUESTIONS_IN_QUIZ]
+    return fields
+
+
+def get_questions(num_questions):
+    questions = []
+    for i in range(1, num_questions+1):
+        question = Question.query.get(i)
+        questions.append(question)
+
+    return questions
+
+
+def create_question_form():
+    if current_user.has_saved_attempt:
+        questions, saved_responses = get_saved_attempt()
+
+        # create quiz form
+        form = QuizForm(
+            question1=saved_responses[0],   # set default values field.default doesn't work
+            question2=saved_responses[1],
+            question3=saved_responses[2]
+        )
+        
+        # get radiofield fields from form
+        fields = get_radio_fields(form)
+
+        index = 0
+        for field in fields:
+            question      = questions[index]
+            field.label   = question.question
+
+            field.choices = [
+                (1, question.response_a), 
+                (2, question.response_b),
+                (3, question.response_c)
+            ]
+
+            index += 1
+
+
+    else:
+        form = QuizForm()
+    
+        fields = get_radio_fields(form)
+
+        questions = get_questions(len(fields))
+
+        index = 0
+        for field in fields:
+            question = questions[index]
+            field.label = question.question
+
+            field.choices = [
+                (1, question.response_a), 
+                (2, question.response_b),
+                (3, question.response_c)
+            ]
+
+            index += 1
+
+    return (form, questions)
+
+
+def submit_attempt(form, questions):
+    if Question.query.get(1).answer == form.question1.data: value_a = 1
+    else: value_a = 0
+    if Question.query.get(2).answer == form.question2.data: value_b = 1
+    else: value_b = 0
+    if Question.query.get(3).answer == form.question3.data: value_c = 1
+    else: value_c = 0
+
+    score = value_a + value_b + value_c
+
+    attempt = SubmittedAttempt( 
+        user_id=current_user.id,
+
+        # change this from being hard coded for question id
+        question_a_id = questions[0].id,
+        response_a=form.question1.data, 
+        mark_a=value_a,
+
+        question_b_id = questions[1].id,
+        response_b=form.question2.data, 
+        mark_b=value_b, 
+
+        question_c_id = questions[2].id,
+        response_c=form.question3.data, 
+        mark_c=value_c,
+            
+        score = score,
+        attempt_datetime = datetime.utcnow()
+    )
+
+    db.session.add(attempt)
+    db.session.commit()
+
+    return score
+
+
+def save_attempt(form, questions):
+    savedAttempt = SavedAttempt(
+        user_id=current_user.id,
+
+        # change this from being hard coded for question id
+        question_a_id  = questions[0].id,
+        response_a     = form.question1.data, 
+
+        question_b_id  = questions[1].id,
+        response_b     = form.question2.data, 
+
+        question_c_id  = questions[2].id,
+        response_c     = form.question3.data, 
+
+        saved_datetime = datetime.utcnow()
+    )
+
+    db.session.add(savedAttempt)
+    current_user.has_saved_attempt = True
+    db.session.commit()
