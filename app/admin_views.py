732bb3abc14a688e1234cd34ef51ee81644e47ee
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-05-03 23:07:42 +0800

    refactored and added comments

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,6 +1,15 @@
 from flask_admin.contrib.sqla import ModelView
 from flask import redirect, url_for
 from flask_login import current_user
+from flask_admin.base import AdminIndexView
+
+
+# make admin page only accessible to admin user
+class MyAdminIndexView(AdminIndexView):
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
 
 class AdminModelView(ModelView):
 
