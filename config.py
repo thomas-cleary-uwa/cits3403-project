21d32026e01434d87d734065f97be311c96be16e
tclearyUWA <51070356+tclearyUWA@users.noreply.github.com>
2021-05-26 14:35:55 +0800

    add postgres database url

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'password123'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://lpsfasifwrqwjl:d4ed880cb09f0eb42c74601dc06491ef4eef1dda21207d307d0b4c65820cf01d@ec2-34-200-94-86.compute-1.amazonaws.com:5432/dche9bn7cuk1m8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # FLASK_ADMIN_SWATCH = "slate"
